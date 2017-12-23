0e0a6c0324a2a0f344d48aa7be284bbe30120e5b
yjjhzxl <yjjhzxl@gmail.com>
2017-12-23 20:45:38 +0800

    update

--- text_class_ch/data_prep.py
+++ text_class_ch/data_prep.py
@@ -6,24 +6,31 @@ from keras.utils import to_categorical
 from sklearn.preprocessing import LabelEncoder,OneHotEncoder
 from keras.preprocessing.sequence import pad_sequences
 
-raw_data = pd.read_csv('TrainSet-eCarX-171019.txt',header=None, delim_whitespace=True,encoding='gbk')
-raw_test = pd.read_csv('TestSet-eCarX-171019.txt',header=None, delim_whitespace=True,encoding='gbk')
+train = pd.read_csv('TrainSet-eCarX-171019.txt',header=None, delim_whitespace=True,encoding='gbk')
+test = pd.read_csv('TestSet-eCarX-171019.txt',header=None, delimiter='#',encoding='gbk')
+del test[1]
+del test[3]
 
+sens = pd.concat([train[1],test[0]])
+labs = pd.concat([train[0],test[2]])
+
+flag = len(train) #flag of train/test margin
+       
 
 encoder = LabelEncoder()
-label = encoder.fit_transform(raw_data[0])
+label = encoder.fit_transform(labs)
 label = label.reshape((len(label),1))
 
 char_dict = {}
 count = 1
-for row in raw_data[1]:
+for row in sens:
     for char in row:
         if char not in char_dict:
             char_dict[char] = count
             count += 1
 
 x_numerical = []
-for row in raw_data[1]:
+for row in sens:
     temp = []
     for char in row:
         temp.append(char_dict[char])
@@ -32,10 +39,20 @@ for row in raw_data[1]:
 x_padded = pad_sequences(x_numerical,maxlen=30,padding='post',truncating='post')
 
 data = np.hstack((x_padded,label))
-np.random.shuffle(data)
 
-X = data[:,:-1]
-Y = data[:,-1]
+Train = data[:flag]
+Test = data[flag:]
 
-np.save('datax',X)
-np.save('datay',Y)
+
+np.random.shuffle(Train)
+np.random.shuffle(Test)
+
+X_train = Train[:,:-1]
+Y_train = Train[:,-1]
+X_test = Test[:,:-1]
+Y_test = Test[:,-1]
+
+np.save('trainx',X_train)
+np.save('trainy',Y_train)
+np.save('testx',X_test)
+np.save('testy',Y_test)
--- text_class_ch/train.py
+++ text_class_ch/train.py
@@ -6,39 +6,51 @@ from sklearn.model_selection import train_test_split
 from keras.models import Sequential
 from keras.layers import Dense,Embedding,Conv1D,MaxPooling1D,BatchNormalization,Dropout,Flatten
 
-X = np.load('datax.npy')
-Y = np.load('datay.npy')
-Y = to_categorical(Y)
+X_train = np.load('trainx.npy')
+Y_train = np.load('trainy.npy')
+X_test = np.load('testx.npy')
+Y_test = np.load('testy.npy')
+'''
+Y_train = to_categorical(Y_train)
+Y_test = to_categorical(Y_test)
+voc_len = np.amax(X_train)
+class_len = Y_train.shape[1]
+'''
 
-voc_len = np.amax(X)
-class_len = Y.shape[1]
-
-
-
-def paral_conv():
-    conv1 = Conv1D(64,3,activation='relu')
-    
-
-
+'''
 model = Sequential()
 model.add(Embedding(voc_len+1,100,input_length=30))
-model.add(Conv1D(128,3,activation='relu',padding='same'))
-model.add(Conv1D(128,3,activation='relu',padding='same'))
+model.add(Conv1D(128,5,activation='relu',padding='same'))
+model.add(Conv1D(128,4,activation='relu',padding='same'))
 model.add(MaxPooling1D(pool_size=2))
-model.add(Conv1D(64,2,activation='relu',padding='same'))
+model.add(BatchNormalization())
+model.add(Conv1D(64,3,activation='relu',padding='same'))
 model.add(Conv1D(64,2,activation='relu',padding='same'))
 model.add(MaxPooling1D(pool_size=2))
 model.add(Dropout(0.2))
 model.add(Flatten())
 model.add(BatchNormalization())
 model.add(Dense(256,activation='relu'))
-model.add(Dropout(0.2))
+model.add(Dropout(0.3))
+model.add(BatchNormalization())
+model.add(Dense(128,activation='relu'))
+model.add(Dropout(0.3))
+model.add(BatchNormalization())
 model.add(Dense(128,activation='relu'))
 model.add(Dropout(0.2))
-model.add(Dense(128,activation='relu'))
-
 model.add(Dense(class_len,activation='softmax'))
 print (model.summary())
 model.compile(loss='categorical_crossentropy',optimizer='adam',metrics=['accuracy'])
-model.fit(X,Y,batch_size=200,epochs=30,validation_split=0.2)
+model.fit(X_train,Y_train,batch_size=200,epochs=20,validation_data=(X_test,Y_test))
 model.save('cnn.h5')
+
+pred = model.predict(X_test)
+'''
+
+def onehot_decode(seq):
+    res = []
+    for row in seq:
+        temp = np.argmax(row)
+        res.append(temp)
+    return res 
+pred = onehot_decode(pred)
